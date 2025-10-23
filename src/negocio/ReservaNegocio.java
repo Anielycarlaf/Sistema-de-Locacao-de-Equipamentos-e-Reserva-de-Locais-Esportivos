052d7d311cc9f1b3f50baddf2804104497372f55
eduardasouzaa021-a <eduardasouzaa021@gmail.com>
2025-10-23 10:42:27 -0300

    ReservaNegocio.java

--- src/negocio/ReservaNegocio.java
+++ src/negocio/ReservaNegocio.java
@@ -1,4 +1,12 @@
 package com.sistemaesportivo.negocio;
 
-public class ReservaNegocio {
+import com.sistemaesportivo.model.Reserva;
+import java.util.List;
+
+public interface ReservaNegocio {
+    Reserva criarReserva(Reserva reserva);
+    Reserva atualizarReserva(Reserva reserva);
+    boolean cancelarReserva(Long id);
+    Reserva buscarPorId(Long id);
+    List<Reserva> listarTodas();
 }
